55725a4563142edc2f4840fba69d643d69b854eb
juanAriel <jvia@ideaelevate.com>
2023-12-22 23:45:46 -0400

    fix bug

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 const Input = () => {
   return (
     <View>
       <Text>hola soy un input</Text>
+      <TextInput/>  
     </View>
   )
 }
