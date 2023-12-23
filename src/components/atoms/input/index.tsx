b111000e64c6d033e9bcc25b297813ce1135110f
juanAriel <jvia@ideaelevate.com>
2023-12-22 23:45:29 -0400

    creacion de la estructura y componentes

---
+++ src/components/atoms/input/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Input = () => {
+  return (
+    <View>
+      <Text>hola soy un input</Text>
+    </View>
+  )
+}
+
+export default Input
