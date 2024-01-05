a50beb26e7629ec410291692b06c2403226beff1
VenturaRichard <venturarichard87@gmail.com>
2024-01-04 21:13:23 -0400

    StartComponent

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -11,85 +11,86 @@ import React from "react";
 import WelcomeProps from "./interface";
 import styled from 'styled-components/native';
 //import Button from "../../components/atoms/button";
+import { Dimensions } from 'react-native';
 
+const { width, height } = Dimensions.get("window");
 
 const ViewContainer  = styled.View`
   background-color: #9DFFFF;
   flex: 1 ;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
+`
+
+const TextHome = styled.Text`
+  justify-content: center;
+  font-size: ${width * 0.1}px;
+  font-weight: bold;
+  margin-bottom: ${height * 0.02}px;
+  color: #000000;
+`
+
+const CircularImage = styled(ImageBackground)`
+  width: ${width * 0.8}px;
+  height: ${width * 0.8}px; /* Hacer la imagen cuadrada */
+  borderRadius: ${width * 0.4}px; /* Hacer el borde circular */
+  overflow: hidden;
+  bottom: ${height * 0.05}px;
+`
+
+const ViewbuttonContainer = styled.View`
+  justify-content: center;
+  align-items: center;  /* Centrar horizontalmente los elementos */
+  flex-direction: row;
+  margin-top: ${height * 0.02}px;
+`
+
+const CustomTouchableOpacity = styled(TouchableOpacity)`
+  width: ${width * 0.4}px;
+  height: ${height * 0.08}px;
+  margin-right: ${width * 0.02}px;  /* Ajusta el margen derecho según sea necesario */
+  margin-left: ${width * 0.02}px;  /* Ajusta el margen izquierdo según sea necesario */
+  border-radius: ${width * 0.05}px;
+  overflow: hidden;
+  background-color: #17BAC0;
+  justify-content: center;
+  align-items: center;
+`
+
+const CustomButtonText = styled.Text`
+  font-size: ${width * 0.05}px;
+  font-weight: 600;
+  line-height: ${height * 0.04}px;
+  letter-spacing: 0;
+  text-align: left;
+  font-family: "Roboto";
+  color: #FFFFFF;
 `
 
 const Welcome: React.FC<WelcomeProps> = ({ navigation }) => {
   return (
     <ViewContainer>
-      <ImageBackground
+      <CircularImage
         imageStyle={{ borderRadius: 250 }}
-        style={styles.imageApp}
         source={require("../../../assets/images/welcome1.png")}
       />
-      <Text style={styles.textHome}> Bienvenido </Text>
+      <TextHome> Bienvenido </TextHome>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.customButtonContainer}
+      <ViewbuttonContainer>
+        <CustomTouchableOpacity
           onPress={() => navigation.navigate("Register")}
         >
-          <Text style={styles.customButtonText}>Registrar</Text>
-        </TouchableOpacity>
+          <CustomButtonText>Registrar</CustomButtonText>
+        </CustomTouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.customButtonContainer}
+        <CustomTouchableOpacity
           onPress={() => navigation.navigate("Login")}
         >
-          <Text style={styles.customButtonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
+          <CustomButtonText>Login</CustomButtonText>
+        </CustomTouchableOpacity>
+      </ViewbuttonContainer>
     </ViewContainer>
   );
 };
 
 export default Welcome;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#9DFFFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textHome: {
-    justifyContent: "center",
-    fontSize: 40,
-    fontWeight: "bold",
-  },
-  imageApp: {
-    width: 350,
-    height: 350,
-    bottom: 50,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    marginTop: 50,
-    justifyContent: "space-between",
-  },
-  customButtonContainer: {
-    width: 184,
-    height: 54,
-    left: 5,
-    marginRight: 10,
-    borderRadius: 20,
-    overflow: "hidden",
-    backgroundColor: "#17BAC0",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  customButtonText: {
-    fontSize: 28,
-    fontWeight: "600",
-    lineHeight: 32,
-    letterSpacing: 0,
-    textAlign: "left",
-    fontFamily: "Roboto",
-    color: "#FFFFFF", 
-  },
-});
