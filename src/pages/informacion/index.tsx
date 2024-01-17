d3707d430e5e57103b7212c7460e0f482df645a7
juanAriel <jvia@ideaelevate.com>
2024-01-16 23:28:39 -0400

    screem information user

--- src/pages/informacion/index.tsx
+++ src/pages/informacion/index.tsx
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { TouchableOpacity } from "react-native";
+import React from "react";
+import styled from "styled-components/native";
 
+const MainViewInfo = styled.View`
+  flex: 1;
+  background-color: #9dffff;
+  align-items: center;
+  justify-content: center;
+`;
+const ViewInfomacionOptions = styled.View`
+  width: 300px;
+  height: 500px;
+  background-color: #32dade;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+  border-radius: 20px;
+`;
+const OptionButton = styled(TouchableOpacity)`
+  width: 280px;
+  height: 54px;
+  border-radius: 20px;
+  background-color: #1599a5;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const TextButton = styled.Text`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+const TextTitle = styled.Text`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 30px;
+  margin-bottom: 50px;
+`;
 const InformacionUsuario = () => {
   return (
-    <View>
-      <Text>InformacionUsuario</Text>
-    </View>
-  )
-}
+    <MainViewInfo>
+      <ViewInfomacionOptions>
+        <TextTitle>Opciones</TextTitle>
+        <OptionButton>
+          <TextButton>Datos usuario</TextButton>
+        </OptionButton>
+        <OptionButton>
+          <TextButton>Añadir mascota</TextButton>
+        </OptionButton>
+        <OptionButton>
+          <TextButton>Ver mascotas</TextButton>
+        </OptionButton>
+        <OptionButton>
+          <TextButton>Salir</TextButton>
+        </OptionButton>
+      </ViewInfomacionOptions>
+    </MainViewInfo>
+  );
+};
 
-export default InformacionUsuario
+export default InformacionUsuario;
